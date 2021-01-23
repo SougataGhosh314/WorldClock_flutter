74208c5fc23b21fe9ec7cbd0b75b27026079f821
Sougata Ghosh <sougataghosh314@gmail.com>
2021-01-24 00:56:09 +0530

    Routing (navigating pages)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:WorldClock_flutter/pages/choose_location.dart';
 import 'package:flutter/material.dart';
 import 'package:WorldClock_flutter/pages/home.dart';
+import 'package:WorldClock_flutter/pages/loading.dart';
 
 void main() => runApp(MaterialApp(
-    home: Home(),
+    initialRoute: "/home",
+    routes: {
+      "/": (context) => Loading(),
+      "/home": (context) => Home(),
+      "/choose_location": (context) => ChooseLocation()
+    },
 ));
